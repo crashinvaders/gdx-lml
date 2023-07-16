78ca176d2fe99c9de3991dd1a27ec379c2b2b581
Anton Chekulaev <metaphore@bk.ru>
2023-07-16 11:43:27 +0200

    Properly handle NPE for LML table attributes.

--- lml/src/main/java/com/github/czyzby/lml/parser/impl/attribute/table/cell/CellExpandYLmlAttribute.java
+++ lml/src/main/java/com/github/czyzby/lml/parser/impl/attribute/table/cell/CellExpandYLmlAttribute.java
@@ -17,13 +17,7 @@ public class CellExpandYLmlAttribute extends AbstractCellLmlAttribute {
     }
 
     protected boolean determineExpandX(final Cell<?> cell) {
-        try {
-            return cell.getExpandX() > 0;
-        } catch (final Exception exception) {
-            // LibGDX Scene2D method returns int, while the field is an Integer that might not have been initiated. This
-            // causes a NPE - so when an exception is thrown, we assume that the expand was not set.
-            Exceptions.ignore(exception);
-            return false;
-        }
+        Integer v = cell.getExpandX();
+        return v != null && v > 0;
     }
 }
