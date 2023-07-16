d11901aa717876bed76a6ef67a8c57b9055f7724
Anton Chekulaev <metaphore@bk.ru>
2023-07-16 22:05:29 +0200

    Revive the "examples" root project and "gdx-autumn-mvc-simple".

--- examples/gdx-autumn-mvc-simple/desktop/src/com/github/czyzby/desktop/DesktopLauncher.java
+++ examples/gdx-autumn-mvc-simple/desktop/src/com/github/czyzby/desktop/DesktopLauncher.java
@@ -1,8 +1,8 @@
 package com.github.czyzby.desktop;
 
 import com.badlogic.gdx.Application;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.github.czyzby.Root;
 import com.github.czyzby.autumn.fcs.scanner.DesktopClassScanner;
 import com.github.czyzby.autumn.mvc.application.AutumnApplication;
@@ -11,16 +11,15 @@ import com.github.czyzby.config.Configuration;
 /** Main entry point for desktop application. */
 public class DesktopLauncher {
     public static void main(final String[] arg) {
-        final LwjglApplicationConfiguration configuration = new LwjglApplicationConfiguration();
-        configuration.title = "Simple Autumn MVC app";
-        configuration.width = Configuration.WIDTH;
-        configuration.height = Configuration.HEIGHT;
+        final Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
+        configuration.setTitle("Simple Autumn MVC app");
+        configuration.setWindowedMode(Configuration.WIDTH, Configuration.HEIGHT);
         createApplication(configuration);
     }
 
-    private static Application createApplication(final LwjglApplicationConfiguration configuration) {
+    private static Application createApplication(final Lwjgl3ApplicationConfiguration configuration) {
         // Note that our ApplicationListener is implemented by AutumnApplication - we just say which classes should be
         // scanned (Configuration.class is the root) and with which scanner (DesktopClassScanner in this case).
-        return new LwjglApplication(new AutumnApplication(new DesktopClassScanner(), Root.class), configuration);
+        return new Lwjgl3Application(new AutumnApplication(new DesktopClassScanner(), Root.class), configuration);
     }
 }
